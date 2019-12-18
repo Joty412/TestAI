949b93419188a84eb0249b35e9dcbda6d90f0baa
Joe <joectaylor1@gmail.com>
2019-12-18 23:13:23 +0000

    Created signal control portion

--- ListTests.py
+++ ListTests.py
@@ -1,5 +1,22 @@
-lists = [False]*5
-lists[2] = True
+# Phases    123456789ABC
+phases = [0b001100001100,
+          0b100001100001,
+          0b000011000011,
+          0b011000010001,
+          0b000000001111,
+          0b000001111000,
+          0b111000000001,
+          0b000001001011,
+          0b001001011000,
+          0b001011000001,
+          0b011000001001,
+          0b001001001001,
+          0b000000000000]
 
-lists.remove(True)
-print(lists)
+# to red
+print(bin(phases[12] & ~ phases[2]))
+# to green
+print(bin(phases[2] & ~ phases[12]))
+
+test = str(bin(phases[0]))
+print([bool(phases[2] & (1 << n)) for n in range(11, -1, -1)])
